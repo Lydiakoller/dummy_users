f8824ddb57dffe49d90f66fef86ed5956381d8c4
Apprentice <apprentice@dbc-chicago-73.local>
2014-01-29 14:27:59 -0600

    .

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,51 @@
+enable :sessions
+
 get '/' do
-  # Look in app/views/index.erb
+  "value = " << session[:value].inspect
+  # signup & #create options link to whichever
   erb :index
 end
+
+get '/signup' do
+
+end
+
+post '/signup' do
+  user = User.create(name: params[:name], email: params[:email], password: params[:password])
+  unless user.errors.any?
+    redirect '/'
+  else
+    redirect '/bad_signup'
+  end
+  # if user creation successful
+  #   redirect to /secret
+  # else show signup error, redirect to '/'
+end
+
+
+post '/login' do
+    authenticated_user = User.authenticate(params[:email], params[:password])
+  if authenticated_user.nil?
+    redirect '/bad_login'
+  else
+    session["email"] = authenticated_user.email
+    redirect '/home'
+  end
+  # if user login successful
+  #   redirect to /secret
+  # else show signup error, redirect to '/'
+end
+
+get '/home' do
+ if session['email'] == nil
+  redirect '/'
+end
+  erb :home
+end
+
+get '/logout' do
+  session['email'] = nil
+  redirect '/'
+end
+
+
